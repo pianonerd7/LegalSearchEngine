05da1ad885bdabc32572c63a199fbd187cd0de47
Anna He <jxh604@case.edu>
2017-04-02 18:13:07 +0800

    some refactoring and added positional indexing for only content

--- index.py
+++ index.py
@@ -27,53 +27,61 @@ def process_documents(file_path, dictionary_file, postings_file):
     doc_length_table = dict()
 
     for filename in collection:
-        new_file_path = file_path + str(filename) + '.xml'
-        xmldoc = ET.parse(new_file_path)
-        nodes = xmldoc.findall('str')
-
-        for node in nodes:
-            attribute = node.attrib['name']
-            if attribute == 'title':
-                title = node.text
-                print title
-            elif attribute == 'content':
-                content = node.text
-                print 'cont'
-            elif attribute == 'court':
-                court = node.text
-                print court
+        (title, content, court, jurisdiction) = parse_xml(file_path, filename)
+        (doc_length, term_index_table) = process_content(content)
         
-        arr = xmldoc.findall('arr')
-
-        for node in arr:
-            attribute = node.attrib['name']
-            if attribute == 'jurisdiction':
-                jurisdiction = node.findall("str")[0].text
-                print jurisdiction
-
-        (term_frequency_table, doc_length) = process_document(new_file_path)
-        update_dictionary(term_frequency_table, filename)
+        update_dictionary(filename, term_index_table)
         doc_length_table[filename] = doc_length
     write_to_disk(dictionary_file, postings_file, doc_length_table, collection)
     print('...index is done building')
 
-# process_document processes the given file and computes a term frequency
-# table for that file and the length of the file.
-def process_document(file):
-    term_frequency_table = dict()
+# parse_xml reads the xml file and gets the useful tags 
+def parse_xml(file_path, filename):
+    new_file_path = file_path + str(filename) + '.xml'
+    xmldoc = ET.parse(new_file_path)
+    nodes = xmldoc.findall('str')
 
-    with open(file, mode="r") as doc:
-        for line in doc:
-            for sent in sent_tokenize(line):
-                for word in word_tokenize(sent):
-                    term = normalize(word)
-                    if term == empty_string:
-                        continue
-                    if term not in term_frequency_table:
-                        term_frequency_table[term] = 0
-                    term_frequency_table[term] += 1
+    for node in nodes:
+        attribute = node.attrib['name']
+        if attribute == 'title':
+            title = node.text
+        elif attribute == 'content':
+            content = node.text
+        elif attribute == 'court':
+            court = node.text
+
+    arr = xmldoc.findall('arr')
+    for node in arr:
+        attribute = node.attrib['name']
+        if attribute == 'jurisdiction':
+            jurisdiction = node.findall("str")[0].text
+
+    return title, content, court, jurisdiction
+
+def process_title(title):
+    print 'hi'
+
+
+# process_content processes the given file and computes a term frequency
+# table for that file and the length of the file.
+def process_content(content):
+    term_frequency_table = dict()
+    term_index_table = dict()
+    index = 0
+    
+    for line in content:
+        for sent in sent_tokenize(line):
+            for word in word_tokenize(sent):
+                term = normalize(word)
+                if term == empty_string:
+                    continue
+                if term not in term_frequency_table:
+                    term_frequency_table[term] = 0
+                    term_index_table[term] = []
+                term_frequency_table[term] += 1
+                term_index_table[term].append(index)
     doc_length = calculate_doc_length(term_frequency_table.values())
-    return (term_frequency_table, doc_length)
+    return (doc_length, term_index_table)
 
 # Calculates the length of the log_tf vector for the document.
 def calculate_doc_length(term_frequencies):
@@ -86,11 +94,11 @@ def calculate_doc_length(term_frequencies):
 # update_dictionary takes the term frequency table as well as the doc id
 # and updates the global dictionary after processing each document in
 # the collection
-def update_dictionary(term_frequency_table, doc_ID):
-    for term in term_frequency_table:
+def update_dictionary(doc_ID, term_index_table):
+    for term in term_index_table:
         if term not in dictionary:
             dictionary[term] = []
-        postings_element = (doc_ID, term_frequency_table[term])
+        postings_element = (doc_ID, term_index_table[term])
         dictionary[term].append(postings_element)
 
 def write_to_disk(dictionary_file, postings_file, doc_length_table, collection):
